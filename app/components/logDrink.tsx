fb2f758703abbfecbc20f2e9cc5610f38d3c5795
MwakaMoogle <dylanmwakasekele@gmmail.com>
2025-11-20 11:33:06 +0000

    Add LogDrink component with basic structure

---
+++ app/components/logDrink.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+const LogDrink = () => {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text>Log Drink Component</Text>
+            <View>
+                <View>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default LogDrink
